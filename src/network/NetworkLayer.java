0d85d82be8a79c990789c9d8207c344c6df253f6
Huub Lievestro <huub.lievestro@nedap.com>
2020-04-02 10:40:13 +0200

    Initial commit of network package

---
+++ src/network/NetworkLayer.java
@@ -0,0 +1,13 @@
+package network;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class NetworkLayer {
+
+	public InetAddress getAdressByName(String hostname) throws UnknownHostException {
+    InetAddress address = InetAddress.getByName(hostname);
+	return address;
+	}
+	
+}
