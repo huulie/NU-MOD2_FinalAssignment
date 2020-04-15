93f99790ea65c79498a0bedaebdf5b27ee53e472
Huub Lievestro <huub.lievestro@nedap.com>
2020-04-15 16:02:18 +0200

    Doc and cleanup protocol package

--- src/main/java/protocol/FileTransferProtocol.java
+++ src/main/java/protocol/FileTransferProtocol.java
@@ -2,14 +2,30 @@ package protocol;
 
 /**
  * Protocol for NU-5 File Transfer (final assignment module 2).
- * This is version: ‘1.0’
+ * This is version: 2020-04-15
  */
 public class FileTransferProtocol {
-
+	// -----------------------------------------------------------------------
+	// Network settings:
+	
 	/**
-	 * Version of this protocol.
+	 * Default port used by client (may be changed).
 	 */
-	public static final String VERSION = "1.0";
+	public static final int DEFAULT_CLIENT_PORT = 1234; 
+	
+	/**
+	 * Default port used by server (note: clients need fixed port).
+	 */
+	public static final int DEFAULT_SERVER_PORT = 4567; 
+	
+	/**
+	 * Maximal size of a Packet (data for the DatagramPacket), consisting of header and payload.
+	 */
+	public static final int MAX_PACKET_SIZE = 1024;
+
+
+	// -----------------------------------------------------------------------
+	// Commands and arguments (send as String in a Packet, objects are send as byte[]):
 	
 	/**
 	 * Delimiter used to separate arguments sent over the network.
@@ -18,144 +34,178 @@ public class FileTransferProtocol {
 	
 	/**
 	 * Signal to indicate unknown message.
+	 * (from either side, indicates that message is not known/understood)
 	 */
 	public static final char ERROR = '?'; 
-			//From either side, indicates that message is not known/understood
 	
 	/**
-	 * TODO .
-	 */
-	public static final int DEFAULT_CLIENT_PORT = 1234; 
-	
-	/**
-	 * TODO .
-	 */
-	public static final int DEFAULT_SERVER_PORT = 4567; 
-	
-	/**
-	 * TODO . discover: String Name
+	 * When send by client: Discover server in local network.
+	 * 	Arguments (separated by delimiter): String nameOfClient
+	 * 	Bytes: -
+	 * 
+	 * When send by server: Respond to client sending DISCOVER in local network.
+	 * 	Arguments (separated by delimiter): String nameOfServer
+	 * 	Bytes: -
 	 */
 	public static final String DISCOVER = "DISCOVER";
 	
 	/**
-	 * TODO .
+	 * When send by client: initialise session with server.
+	 * 	Arguments (separated by delimiter): String nameOfClient
+	 * 	Bytes: -
+	 * 
+	 * When send by server: Respond to client sending INIT.
+	 * 	Arguments (separated by delimiter): int serverPort, running clientHandler
+	 * 	Bytes: -
 	 */
 	public static final String INIT_SESSION = "INIT"; 
 	
 	/**
-	 * TODO . only in current directory
+	 * When send by client: request list of file on server.
+	 * 	Arguments (separated by delimiter): -
+	 * 	Bytes: -
+	 * 
+	 * When send by server: Respond to client sending LIST_FILES.
+	 * 	Arguments (separated by delimiter): -
+	 * 	Bytes: File[] fileArray with all files in server storage
 	 */
 	public static final String LIST_FILES = "ls";
 	
 	/**
-	 * TODO download: dowloadHelper port + file (object as bytes); 
+	 * When send by client: request download of file from server.
+	 * 	Arguments (separated by delimiter): int portOfDownloader
+	 * 	Bytes: File fileToDownload
+	 * 
+	 * When send by server: Respond to client sending DOWNLOAD.
+	 * 	Arguments (separated by delimiter): 
+	 * 		int portOfUploader; int totalFileSize (in bytes); int startID
+	 * 	Bytes: File[] fileToDownload (to client)
 	 */
 	public static final String DOWNLOAD = "DOWNLOAD";
 	
 	/**
-	 * TODO upload: uploadHelper port; int startID, file (object as bytes)  
+	 * When send by client: request upload of file to server.
+	 * 	Arguments (separated by delimiter): 
+	 * 		int portOfUploader; int totalFileSize (in bytes); int startID
+	 * 	Bytes: File[] fileToUpload 
+	 * 
+	 * When send by server: Respond to client sending UPLOAD.
+	 * 	Arguments (separated by delimiter): int portOfDownloader
+	 * 	Bytes: File fileToUpload (to server)
 	 */
 	public static final String UPLOAD = "UPLOAD";
 	
 	/**
-	 * TODO delete: file (object as bytes) 
+	 * When send by client: request deletion of file on server.
+	 * 	Arguments (separated by delimiter): -
+	 * 	Bytes: File[] fileToDelete 
+	 * 
+	 * When send by server: Respond to client confirming DELETE.
+	 * 	Arguments (separated by delimiter): -
+	 * 	Bytes: File fileToDelete (on server)
 	 */
 	public static final String DELETE = "DELETE";
 	
 	/**
-	 * TODO hash: hash (as string) + file (object as bytes); 
+	 * When send by client: request hash of file on server.
+	 * 	Arguments (separated by delimiter): String localHash
+	 * 	Bytes: File[] fileToCheck 
+	 * 
+	 * When send by server: Respond to client sending HASH.
+	 * 	Arguments (separated by delimiter): String remoteHash
+	 * 	Bytes: File fileToCheck (on server)
 	 */
 	public static final String HASH = "HASH";
 	
 	/**
-	 * TODO .
+	 * When send by client: NOT USED (YET)
+	 * 
+	 * When send by server: server has failed.
+	 * 	Arguments (separated by delimiter): String failureMessage
+	 * 	Bytes: -
+	 */
+	public static final String FAILED = "FAILED";
+
+
+	// -----------------------------------------------------------------------
+		// Up-/download helper communication (send as byte[] in a Packet):
+	/**
+	 * From downloader to uploader: start transmission of bytes
+	 * (used when downloader has to contact uploader first, e.g. trough firewalls)
 	 */
 	public static final byte[] START_DOWNLOAD = "START".getBytes(); 
 	
 	/**
-	 * TODO .
+	 * Mostly from downloader to uploader (but may be used v.v.): packet received!
 	 */
 	public static final byte[] ACK = "ACK".getBytes(); 
 	
 	/**
-	 * TODO .
+	 * Mostly from downloader to uploader (but may be used v.v.): pause transfer.
 	 */
 	public static final byte[] PAUSE_DOWNLOAD = "PAUSE".getBytes(); 
 	
 	/**
-	 * TODO .
+	 * Mostly from downloader to uploader (but may be used v.v.): resume transfer.
 	 */
 	public static final byte[] RESUME_DOWNLOAD = "RESUME".getBytes();
-	
-	
-	/**
-	 * TODO to indicate operation on server failed
-	 * TODO implement
-	 */
-	public static final String FAILED = "FAILED";
 
-	
-	/**
-	 * TODO
-	 */
-	// PACKET DATA FORMAT: header (x bytes) + payload (maximum size: PACKET_SIZE - x bytes)
-	// header (bytes): id (), headerLength ( = , byteOffset()
-	// NOTE: number of bytes of payloadLength field is dependent on PACKET_SIZE: length (in bytes) = log2(PACKET_SIZE)/8
-	// NOTE: part of the datagram header (like address and port) is managed by the used libraries, and thus not included here.
 
-	/**
-	 * TODO .
-	 */
-	public static final int MAX_PACKET_SIZE = 1024; // TODO was 256
+	// -----------------------------------------------------------------------
+	// Packet header format (send as bytes in a DatagramPacket, to extend datagram header)
 	
 	/**
-	 * TODO . 
-	 * NOTE: ID should be encoded with big-endian encoding
-	 * NOTE: ID may not be larger than 4 bytes = 4*8 bits => (4*8)^2 = 1024
+	 * Header field: id. 
+	 * 	Length: 4 bytes (the int2Byte always puts in block of 4 bytes)
+	 * 	Representing: id of Packet
+	 * 	NOTE: ID should be encoded with big-endian encoding
+	 * 	NOTE: ID may not be larger than MAX_ID
 	 */
 	public static final int HEADER_ID_START = 0;
-	public static final int HEADER_ID_LAST = HEADER_ID_START 
-			//+ (int) Math.ceil(Math.log(MAX_PACKET_SIZE) / Math.log(2) / 8);
-			+ 3; // TODO the int2Byte always puts in block of 4 bytes
+	public static final int HEADER_ID_LAST = HEADER_ID_START + 3; 
 	
 	
 	/**
-	 * TODO . 
-	 * NOTE: payload length should be encoded with big-endian encoding
-	 * NOTE: may not be larger than 4 bytes = 4*8 bits => (4*8)^2 = 1024
+	 * Header field: header length. 
+	 * 	Length: 4 bytes (the int2Byte always puts in block of 4 bytes)
+	 * 	Representing: lenght of header
+	 * 	NOTE: header length should be encoded with big-endian encoding
+	 * 	NOTE: value may not be larger than 4 bytes [= 2^(4*8)]
 	 */
 	public static final int HEADER_HEADER_LENGTH_START = HEADER_ID_LAST + 1;
-	public static final int HEADER_HEADER_LENGTH_LAST = HEADER_HEADER_LENGTH_START 
-			//+ (int) Math.ceil(Math.log(MAX_PACKET_SIZE) / Math.log(2) / 8);
-			+ 3; // TODO the int2Byte always puts in block of 4 bytes 
+	public static final int HEADER_HEADER_LENGTH_LAST = HEADER_HEADER_LENGTH_START + 3; 
 	
 	/**
-	 * TODO . 
-	 * NOTE: payload length should be encoded with big-endian encoding
+	 * Header field: byte offset. 
+	 * 	Length: 4 bytes (the int2Byte always puts in block of 4 bytes)
+	 * 	Representing: offset of bytes in payload of Packet (after String payload)
+	 * 	NOTE: byte offset should be encoded with big-endian encoding
+	 *  NOTE: value may not be larger than 4 bytes [= 2^(4*8)]
 	 */
 	public static final int HEADER_BYTE_OFFSET_START = HEADER_HEADER_LENGTH_LAST + 1;
-	public static final int HEADER_BYTE_OFFSET_LAST = HEADER_BYTE_OFFSET_START 
-			//+ (int) Math.ceil(Math.log(MAX_PACKET_SIZE) / Math.log(2) / 8);
-			+ 3; // TODO the int2Byte always puts in block of 4 bytes 
+	public static final int HEADER_BYTE_OFFSET_LAST = HEADER_BYTE_OFFSET_START + 3;
 
 	/**
-	 * TODO .
-	 * set total header size to last assigned header field, in bytes
+	 * Actual value of total header size: set to last assigned header field, in bytes.
 	 * NOTE: plus one, because indices start at zero (and lengths at one)
-	 * NOTE: may not be larger than 4 bytes = 4*8 bits => (4*8)^2 = 1024
+	 * NOTE: this length may not be larger than 4 bytes [= 2^(4*8)]
 	 */
 	public static final int TOTAL_HEADER_SIZE = HEADER_BYTE_OFFSET_LAST + 1;
 	
+	/**
+	 * Index of first byte of the payload, starting after the header. 
+	 * (note: header size is length, so already plus one)
+	 */
 	public static final int PAYLOAD_START = TOTAL_HEADER_SIZE; // TODO header size already plus one
 	
-
 	/**
-	 * TODO *8bits per bytes, to power two,  note max capacity of int (all over protocol!!)
+	 * Maximal value of the ID field in the header (based on field size in bytes).
 	 */
-	public static final int MAX_ID = (int) Math.pow(2, (double)((HEADER_ID_LAST - HEADER_ID_START+1) * 8))-1;
+	public static final int MAX_ID = (int) 
+			Math.pow(2, (double) ((HEADER_ID_LAST - HEADER_ID_START + 1) * 8)) - 1;
 	
 	/**
-	 * TODO .
+	 * Maximal value of the payload in a Packet (based on max packetSize - headerSize).
 	 */
 	public static final int MAX_PAYLOAD_LENGTH = MAX_PACKET_SIZE - TOTAL_HEADER_SIZE; 
 }
